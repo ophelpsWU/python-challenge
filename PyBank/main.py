0283d0554c98d2d861686aaa10ac7acf3b0e2b8f
Owen Phelpsgit config --global user.email ophelps@wustl.edugit config --global user.name Owen <ophelps@wustl.edu>
2023-09-28 19:05:57 -0500

    Fix for checking for PL changes

--- PyBank/main.py
+++ PyBank/main.py
@@ -4,10 +4,14 @@ import csv
 # Get the path to the csv to read
 csvpath = os.path.join("Resources", "budget_data.csv")
 
+# create variables
 totalPL = 0
+totalDelta = 0
 icount = 0
-maxPL = 0
-minPL = 0
+maxDelta = 0
+minDelta = 0
+prevPL = 0
+delta = 0
 
 # Open the CSV using the UTF-8 encoding
 with open(csvpath, encoding='UTF-8') as csvfile:
@@ -18,24 +22,33 @@ with open(csvpath, encoding='UTF-8') as csvfile:
     for row in csvreader:
         icount +=1
         linePL = int(row[1])
+        
+        if icount == 1:
+            totalDelta -= linePL
+        else:
+            delta = linePL - prevPL
+
         totalPL+=linePL
-        if minPL > linePL:
+        if minDelta > delta:
             minMonth = row[0]   
-            minPL = linePL
-        if maxPL < linePL:
-            maxPL = linePL
+            minDelta = delta
+        if maxDelta < delta:
+            maxDelta = delta
             maxMonth = row[0]
+        prevPL = int(row[1])
 
 # calculate the average
-average = round(totalPL / icount,2)
+totalDelta += prevPL
+
+average = round(totalDelta / (icount - 1),2)
 
 # assemble the output string for the analysis
 strOutput = "Financial Analysis\n----------------------------"
 strOutput += "\nTotal Months: " + str(icount) 
 strOutput += "\nTotal: $" + str(totalPL) 
 strOutput += "\nAverage Change: $" + str('{0:.2f}'.format(average))
-strOutput += "\nGreatest Increase in Profits: " + maxMonth + " (" + str(maxPL) + ")" 
-strOutput += "\nGreatest Decrease in Profits: " + minMonth + " (" + str(minPL) + ")"
+strOutput += "\nGreatest Increase in Profits: " + maxMonth + " (" + str(maxDelta) + ")" 
+strOutput += "\nGreatest Decrease in Profits: " + minMonth + " (" + str(minDelta) + ")"
 
 
 # write the output string to a file
